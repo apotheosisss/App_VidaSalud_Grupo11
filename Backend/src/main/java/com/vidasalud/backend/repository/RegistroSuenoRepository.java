e0c8cba6e6685c1f5a652a7abd0f969eed22645c
Brian <163011832+brx2b@users.noreply.github.com>
2025-12-03 22:16:58 -0300

    Delete Backend directory

--- Backend/src/main/java/com/vidasalud/backend/repository/RegistroSuenoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vidasalud.backend.repository;
-
-import com.vidasalud.backend.model.RegistroSueno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RegistroSuenoRepository extends JpaRepository<RegistroSueno, Long> {
-    List<RegistroSueno> findByUsuarioId(Long usuarioId);
-}
